caa66ccf59d0365ffbe2f5bd4b17bbe018a727f6
prakash pawar <31501098+thevenice@users.noreply.github.com>
2020-01-28 19:01:54 +0530

    v4

---
+++ precache-manifest.d25c3598595ad9d7dd180d1483394753.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0320bc828d6caa80859aa0d00b5c04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6451d3445de13a674aa",
+    "url": "/static/css/main.ea3b1a5a.chunk.css"
+  },
+  {
+    "revision": "cf45ca9b7832908d53d4",
+    "url": "/static/js/2.070ff29e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.070ff29e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6451d3445de13a674aa",
+    "url": "/static/js/main.b9514a1d.chunk.js"
+  },
+  {
+    "revision": "10bee7c4297498bff5b2",
+    "url": "/static/js/runtime-main.4784d7de.js"
+  },
+  {
+    "revision": "b9a8f90e7bf16b9297ede5146a69f530",
+    "url": "/static/media/SocialBottom.b9a8f90e.png"
+  },
+  {
+    "revision": "b24bb1d006f5870ff6732bd8edffc65d",
+    "url": "/static/media/branding_man.b24bb1d0.jpg"
+  },
+  {
+    "revision": "9389da729cd6a1810ef65c5981cca3c6",
+    "url": "/static/media/choices-colour.9389da72.svg"
+  },
+  {
+    "revision": "2355471805b66222839d57adc396bb4c",
+    "url": "/static/media/event.23554718.png"
+  },
+  {
+    "revision": "a711c867314978e9200a9560646e94a9",
+    "url": "/static/media/handshake-colour.a711c867.svg"
+  },
+  {
+    "revision": "bcba4c09469442aaaeef3086911e90d5",
+    "url": "/static/media/onad.bcba4c09.png"
+  }
+]);
